80ad90f7aff390bc60585aea5e7404ae1a4db771
Fedor Nezhivoy <gyzerok@gmail.com>
2016-05-17 18:31:34 +0600

    fail example

--- src/quiz/modules/Question/Input.elm
+++ src/quiz/modules/Question/Input.elm
@@ -30,6 +30,7 @@ decoder =
 type Msg
   = Input String
   | Submit
+  | NoOp
 
 
 type Shout
@@ -38,12 +39,17 @@ type Shout
 
 update : Msg -> Model -> ( Model, List Shout )
 update msg model =
-  case msg of
+  case (Debug.log "Message" msg) of
     Input str ->
       ( { model | input = str }, [] )
 
     Submit ->
-      ( model, [ OnSubmit ( model.question.title, model.input ) ] )
+      ( Debug.log "Submit" model, [] )--[ OnSubmit ( model.question.title, model.input ) ] )
+
+    NoOp ->
+      ( Debug.log "NoOp" model, [] )
+
+
 
 
 view : Bool -> Model -> Html Msg
@@ -53,6 +59,6 @@ view isHidden model =
     [ h2 [] [ text model.question.title ]
     , textarea [ onInput Input, value model.input ] []
     , button
-      [ onClick Submit ]
+      []--[ onClick Submit ]
       [ text "Submit" ]
     ]
